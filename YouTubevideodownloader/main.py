57139ba814b788747b767655e625ac005db27c6e
waltertaya <waltermittybecker5@gmail.com>
2024-06-20 11:10:53 +0300

    Updated: Script

--- YouTubevideodownloader/main.py
+++ YouTubevideodownloader/main.py
@@ -1,4 +1,7 @@
+#!/usr/bin/env python3
+
 from pytube import YouTube
+import sys
 
 def download_video(url):
     try:
@@ -9,6 +12,10 @@ def download_video(url):
     except Exception as e:
         print("An error occurred while downloading the video:", str(e))
 
-# Example usage
-video_url = "https://www.youtube.com/shorts/toZ35X_I_XI"  # Replace with your video URL
-download_video(video_url)
+
+if __name__ == '__main__':
+    if (len(sys.argv) == 2):
+        url = sys.argv[1]
+    else:
+        url = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+    download_video(url)
